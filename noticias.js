60b3637bb5e8c2e6edccececdd0ce70cfbb4b4f3
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-20 21:14:25 -0300

    HTTP Requests in Node.js

--- noticias.js
+++ noticias.js
@@ -1,1 +1,7 @@
-console.log('Portal de notícias - teste');
+let http = require('http');
+
+let server = http.createServer( function(req, res) {
+  res.end("<html><body>Portal de notícias</body></html>");
+}); 
+
+server.listen(3000);
