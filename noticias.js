990ff081477f0852b4595ce6bdc6eea6c9f4c287
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-20 21:20:10 -0300

    HTTP requests with url parameters

--- noticias.js
+++ noticias.js
@@ -1,7 +1,15 @@
 let http = require('http');
 
 let server = http.createServer( function(req, res) {
-  res.end("<html><body>Portal de notícias</body></html>");
+  let category = req.url;
+
+  switch (category) {
+    case '/tecnologia': res.end("<html><body>Notícias de Tecnologia</body></html>");break;
+    case '/moda': res.end("<html><body>Notícias de Moda</body></html>");break;
+    case '/beleza': res.end("<html><body>Notícias de Beleza</body></html>");break;
+    default: res.end("<html><body>Portal de notícias</body></html>");
+  }
+  
 }); 
 
 server.listen(3000);
