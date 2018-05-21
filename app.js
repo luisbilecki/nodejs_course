e91f3d78d17cc1eb930da9df8f1d626d451c2052
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-20 21:45:12 -0300

    Refactor app.js routes

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ let app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/tecnologia', function(req,res) {
-  res.render("secao/tecnologia");
-});
-
 app.get('/', function(req,res) {
-  res.send("<html><body>Portal de noticias</body></html>");
+  res.render("home/index");
 });
 
+app.get('/formulario-inclusao-noticia', function(req,res) {
+  res.render("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req,res) {
+  res.render("noticias/noticias");
+});
+
+app.get('/noticia', function(req,res) {
+  res.render("noticias/noticia");
+});
 
 app.listen(3000, function() {
   console.log("Servidor rodando com Express");
