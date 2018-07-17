f6c4f09d92600ef0a4aa9655781410393da9dca4
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-07-16 21:35:50 -0300

    [Multiroomchat] - Add form validation in chat sign in

--- multiroom_chat/app/controllers/chat.js
+++ multiroom_chat/app/controllers/chat.js
@@ -1,4 +1,14 @@
 const iniciaChat = (app, req, res) => {
+  req.assert('apelido', 'Nome ou Apelido é obrigatório').notEmpty();
+  req.assert('apelido', 'Nome ou Apelido deve conter entre 3 e 15 caracteres').len(3,15);
+
+  let errors = req.validationErrors();
+
+  if(errors){
+      res.render("index", { validacao : errors });
+      return;
+  }
+
   res.render('chat');
 };
 
--- multiroom_chat/app/controllers/index.js
+++ multiroom_chat/app/controllers/index.js
@@ -1,5 +1,5 @@
 const home = (app, req, res) => {
-  res.render('index');
+  res.render('index', { validacao : {} });
 };
 
 module.exports = {
