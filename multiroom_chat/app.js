9dae9437622abfdd74ce9edbebf412cb45151eac
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-07-16 22:34:34 -0300

    [Multiroomchat] - Setup socket.io in chat project

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -1,7 +1,15 @@
 const app = require('./config/server');
 
-let server = app.listen(8080, function(){
+let server = app.listen(8000, () => {
     console.log('Servidor online');
 })
 
-require('socket.io').listen(server);
+let io = require('socket.io').listen(server);;
+
+io.on('connection', (socket) => {
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectou');
+    });
+});
