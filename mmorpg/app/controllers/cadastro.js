68028c3e8956b32344f05312bf608db462f419d5
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-08-13 20:17:43 -0300

    [MMORPG] - Add POST route to insert new player

--- mmorpg/app/controllers/cadastro.js
+++ mmorpg/app/controllers/cadastro.js
@@ -1,5 +1,25 @@
 const cadastro = (app, req, res) => {
-  res.render('cadastro');
+  res.render('cadastro', { validacao: {}, dadosForm: {} });
 };
 
-module.exports = { cadastro };
+const cadastrar = (app, req, res) => {    
+  const dadosForm = req.body;
+
+  req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+  req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+  req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+  req.assert('casa', 'Casa não pode ser vazia').notEmpty();
+
+  const erros = req.validationErrors();
+
+  if (erros) {
+      res.render('cadastro', { validacao: erros, dadosForm: dadosForm });
+      return;
+  }
+
+  res.send('podemos cadastrar');
+}
+module.exports = { 
+  cadastro,
+  cadastrar 
+};
