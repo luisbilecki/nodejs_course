f5d468f72bebf3d5fb30d8b820c9d8339c9bc75a
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-08-13 19:35:46 -0300

    [MMORPG] - Initial setup

---
+++ mmorpg/config/server.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+	consign = require('consign'),
+	bodyParser = require('body-parser'),
+	expressValidator = require('express-validator'),
+	app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+app.use(express.static('./app/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+consign()
+	.include('app/routes')
+	.then('app/models')
+	.then('app/controllers')
+	.into(app);
+
+module.exports = app;
