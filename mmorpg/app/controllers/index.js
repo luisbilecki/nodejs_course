9a9281a1e2c840c1ca043b21df27cbaaee444de7
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-09-01 20:25:02 -0300

    Add authentication action in index js controller

--- mmorpg/app/controllers/index.js
+++ mmorpg/app/controllers/index.js
@@ -2,4 +2,24 @@ const index = (application, req, res) => {
   res.render('index');
 };
 
-module.exports = { index };
+const autenticar = (application, req, res) => {    
+  const dadosForm = req.body;
+
+  req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+  req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+
+  const erros = req.validationErrors();
+
+  if(erros){
+      res.render('index', { validacao: erros });
+      return;
+  }
+
+  res.send('tudo ok para criar a sessão');
+
+}
+
+module.exports = { 
+  index,
+  autenticar
+};
