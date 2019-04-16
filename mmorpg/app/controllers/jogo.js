aedfdf08e701ef4a77301882626f99f6617f752c
Luis F. Bilecki <luis.bilecki@gmail.com>
2019-04-15 22:51:08 -0300

    [MMORPG] Dynamic flag for user house

--- mmorpg/app/controllers/jogo.js
+++ mmorpg/app/controllers/jogo.js
@@ -1,6 +1,6 @@
 const jogo = (app, req, res) => {
   if(req.session.autorizado) {
-    res.render('jogo');
+    res.render('jogo', { img_casa: req.session.casa });
   } else{
     res.send('Usuário precisa fazer login');
   }
