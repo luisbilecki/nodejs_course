8ac6e55f2310d86145f296cfb5c67505fec71103
Luis F. Bilecki <luis.bilecki@gmail.com>
2019-04-15 23:30:49 -0300

    [MMORPG] End of project
    This commit ends the MMORPG project.
    Further improvements and refactoring will be needed, because this code uses an older version of
    mongodb package that is not more supported today.

--- mmorpg/app/controllers/jogo.js
+++ mmorpg/app/controllers/jogo.js
@@ -4,6 +4,11 @@ const jogo = (app, req, res) => {
     return;
   }
 
+  const msg = '';
+	if(req.query.msg != ''){
+		msg = req.query.msg;
+	}
+
   const { usuario, casa } = req.session;
 
   const connection = application.config.dbConnection;
@@ -19,15 +24,69 @@ const sair = (app, req, res) => {
 };
 
 const suditos = (app, req, res) => {
-  res.render('aldeoes', { validacao: {} });
+	if (!req.session.autorizado) {
+		res.send('Usuário precisa fazer login');
+		return;	
+	}
+
+	res.render("aldeoes", {validacao: {}});
 }
 
-const pergaminhos = (app, req, res) => {
-  res.render('pergaminhos', { validacao: {} });
+const pergaminhos = (app, req, res) => {  
+	if (!req.session.autorizado) {
+		res.send('Usuário precisa fazer login');
+		return;	
+	}
+
+	const connection = application.config.dbConnection;
+	const JogoDAO = new application.app.models.JogoDAO(connection);
+
+	const usuario = req.session.usuario;
+
+	JogoDAO.getAcoes(usuario, res);
 }
+
+const ordenar_acao_sudito = (app, req, res) => {  
+	if (req.session.autorizado !== true) {
+		res.send('Usuário precisa fazer login');
+		return;	
+	}
+	
+	const dadosForm = req.body;
+
+	req.assert('acao', 'Ação deve ser informada').notEmpty();
+	req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
+
+	const erros = req.validationErrors();
+
+	if (erros) {
+		res.redirect('jogo?msg=A');
+		return;
+	}
+
+	const connection = application.config.dbConnection;
+	const JogoDAO = new application.app.models.JogoDAO(connection);
+
+	dadosForm.usuario = req.session.usuario;
+	JogoDAO.acao(dadosForm);
+
+	res.redirect('jogo?msg=B');
+};
+
+const revogar_acao = (app, req, res) => {
+  const { id_acao } = req.query;
+
+	const connection = application.config.dbConnection;
+	const JogoDAO = new application.app.models.JogoDAO(connection);
+
+	JogoDAO.revogarAcao(id_acao, res);
+};
+
 module.exports = { 
   jogo,
   sair,
   suditos,
   pergaminhos,
+  ordenar_acao_sudito,
+  revogar_acao,
 };
--- mmorpg/app/models/JogoDAO.js
+++ mmorpg/app/models/JogoDAO.js
@@ -1,4 +1,4 @@
-function JogoDAO(connection){
+function JogoDAO(connection) {
   this._connection = connection();
 }
 
@@ -22,9 +22,9 @@ JogoDAO.prototype.gerarParametros = function(usuario) {
 
 JogoDAO.prototype.iniciaJogo = function(res, usuario, casa) {
     this._connection.open(function(err, mongoclient) {
-        mongoclient.collection("jogo", function(err, collection) {
+        mongoclient.collection('jogo', function(err, collection) {
             collection.find({usuario:usuario}).toArray(function(err, result) {
-                res.render('jogo', { img_casa: casa, jogo: result[0] });
+                res.render('jogo', { img_casa: casa, jogo: result[0], msg: msg });
             });
 
             mongoclient.close();
@@ -32,6 +32,78 @@ JogoDAO.prototype.iniciaJogo = function(res, usuario, casa) {
     });    
 }
 
-module.exports = function(){
+
+JogoDAO.prototype.acao = function(acao) {
+	this._connection.open( function(err, mongoclient) {
+		mongoclient.collection('acao', function(err, collection) {
+			
+			let date = new Date();
+
+			let tempo = null;
+			
+			switch(parseInt(acao.acao)) {
+				case 1: tempo = 1 * 60 * 60000; break;
+				case 2: tempo = 2 * 60 * 60000; break;
+				case 3: tempo = 5 * 60 * 60000; break;
+				case 4: tempo = 5 * 60 * 60000; break;
+			}	
+
+			acao.acao_termina_em = date.getTime() + tempo;
+			collection.insert(acao);
+
+		});
+
+		mongoclient.collection('jogo', function(err, collection) {
+
+			let moedas = null;
+
+			switch(parseInt(acao.acao)) {
+				case 1: moedas = -2 * acao.quantidade; break;
+				case 2: moedas = -3 * acao.quantidade; break;
+				case 3: moedas = -1 * acao.quantidade; break;
+				case 4: moedas = -1 * acao.quantidade; break;
+			}	
+
+			collection.update(
+				{ usuario: acao.usuario},
+				{ $inc: {moeda: moedas}}
+			);
+
+			mongoclient.close();
+		});
+	});
+}
+
+JogoDAO.prototype.getAcoes = function(usuario, res) {
+	this._connection.open( function(err, mongoclient) {
+		mongoclient.collection('acao', function(err, collection) {			
+			let date = new Date();
+			let momento_atual = date.getTime();
+
+			collection.find({usuario : usuario, acao_termina_em: {$gt:momento_atual}}).toArray(function(err, result) {
+				
+				res.render('pergaminhos', {acoes: result});
+				
+				mongoclient.close();
+			});
+		});
+	});
+}
+
+JogoDAO.prototype.revogarAcao = function(_id, res) {
+	this._connection.open(function(err, mongoclient) {
+		mongoclient.collection('acao', function(err, collection) {
+			collection.remove(
+				{_id : ObjectID(_id)},
+				function(err, result) {
+					res.redirect('jogo?msg=D');
+					mongoclient.close();
+				}
+			);			
+		});
+	});
+}
+
+module.exports = function() {
   return JogoDAO;
 }
