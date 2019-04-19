038a8b4e0a0ab0a581bbc9c14e27115845e7f859
Luis F. Bilecki <luis.bilecki@gmail.com>
2019-04-19 15:07:32 -0300

    [Instagram clone] Inserting new posts

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -1,5 +1,7 @@
 const express = require('express'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  multiparty = require('connect-multiparty'),
+  fs = require('fs');
 
 const { connMongoDB } = require('./db');
 const objectID = require('mongodb').ObjectId;
@@ -11,12 +13,27 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
+app.use(multiparty());
 
 const port = 8080;
 
 app.listen(port); 
 
 console.log('Server listening on ' + port);
+
+// CORS
+const cors = require('cors');
+app.use(cors({
+  origin: ['*'], // In production we should change this to frontend url
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+}));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 
 // GET (hello)
 app.get('/', (req, res) => {
@@ -25,19 +42,34 @@ app.get('/', (req, res) => {
 
 // POST (create)
 app.post('/api', (req, res) => {
-  const bodyData = req.body;
+  const imgTs = new Date().getTime();
+  const imgUrl = `${imgTs}_${req.files.arquivo.originalFilename}`;
+  const originPath = req.files.arquivo.path;
+  const destinationPath = `./uploads/${imgUrl}`;
 
-  connMongoDB({
-    operation: 'insert',
-    reqData: bodyData,
-    collection: 'posts',
-    callback: function(err, records){
-      if (err) {
-        res.json({ status: 'erroed', err });
-      } else {
-        res.json({ status: 'posts inserted successfully' });
-      }
+  fs.rename(originPath, destinationPath, (err) => {
+    if(err){
+        res.status(500).json({ error : err });
+        return;
     }
+
+    const dataToInsert = {
+        url_imagem: imgUrl,
+        titulo: req.body.titulo
+    }
+
+    connMongoDB({
+      operation: 'insert',
+      reqData: dataToInsert,
+      collection: 'posts',
+      callback: function(err, records){
+        if (err) {
+          res.json({ status: 'erroed', err });
+        } else {
+          res.json({ status: 'posts inserted successfully' });
+        }
+      }
+    });
   });
 });
 
